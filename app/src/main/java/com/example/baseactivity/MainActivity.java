7b2f60f0f32c595ee9f70919bcb11c794b73e400
Savino Yu <215856165@qq.com>
2016-10-18 21:06:32 +0800

    两种不同的写Toast的方式。被我写进了BaseActivity

--- app/src/main/java/com/example/baseactivity/MainActivity.java
+++ app/src/main/java/com/example/baseactivity/MainActivity.java
@@ -1,13 +1,43 @@
 package com.example.baseactivity;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
+
+    private Button button1,button2;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    protected int getContentViewId() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    protected void initViews() {
+
+    }
+
+    @Override
+    protected void initFindViewById() {
+        button1 = (Button) findViewById(R.id.id_button1);
+        button2 = (Button) findViewById(R.id.id_button2);
+    }
+
+    @Override
+    protected void setListener() {
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "1<---多次点击，看看多久消失--->1", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showToast("2<---多次点击，看看多久消失--->2");
+            }
+        });
     }
 }
