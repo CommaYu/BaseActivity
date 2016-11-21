c203432ab9edd52477a410d9d75f3fd458c2a34a
SavinoYu <215856165@qq.com>
2016-11-21 13:24:30 +0800

    Update Common.java

--- app/src/main/java/com/example/baseactivity/Common.java
+++ app/src/main/java/com/example/baseactivity/Common.java
@@ -4,7 +4,7 @@ import android.content.Context;
 import android.graphics.Typeface;
 
 /**
- * Created by ysc on 10/24/2016.
+ * Created by ysc on 10/24/2016.貌似这么一直用会让我的APP占用内存很大，不知道什么原因
  */
 
 public class Common {
